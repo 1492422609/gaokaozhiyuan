e55cae822f134d04b47ff01b37cac5a35ea5ee5e
1492422609 <1492422609@qq.com>
2021-01-26 18:33:00 +0800

    添加订单页面

--- pages/tianxiechengji/index.js
+++ pages/tianxiechengji/index.js
@@ -1,9 +1,7 @@
-// pages/tianxiechengji/index.js
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+  
   data: {
     array: ['普通招生', '不普通招生'],
     objectArray: [
@@ -115,58 +113,9 @@ Page({
     this.setData(data);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ queding(){
+   wx.navigateBack({
+     delta: 0,
+   })
+ }
 })
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,56 +1,3 @@
-function t(t) {
-    var e = "";
-    switch (t) {
-      case 0:
-        e = "校园服务";
-        break;
-
-      case 1:
-        e = "青春伴夕阳";
-        break;
-
-      case 2:
-        e = "未来梦工程";
-        break;
-
-      case 3:
-        e = "助残阳光行动";
-        break;
-
-      case 4:
-        e = "社区公益";
-        break;
-
-      case 5:
-        e = "文化宣传";
-        break;
-
-      case 6:
-        e = "医疗保健";
-        break;
-
-      case 7:
-        e = "应急救护";
-        break;
-
-      case 8:
-        e = "地球卫士";
-        break;
-
-      case 9:
-        e = "校院赛会";
-        break;
-
-      default:
-        e = "志愿活动";
-    }
-    return e;
-}
-
-require("../../zong_js/common");
-
-var e = require("../../zong_js/common"), a = require("../../zong_js/util"), i = getApp(), s = wx.getStorageSync("my_nick"), n = wx.getStorageSync("my_sex"), o = wx.getStorageSync("my_avatar");
-
 Page({
   handleGetUserInfo(e) {
     const {
@@ -64,15 +11,5 @@ Page({
 onShow(){
   const userinfo=wx.getStorageSync("userinfo");
   this.setData({userinfo})
-  this.onLoad(), console.log("加载头像");
-  var e = this;
-  wx.getUserInfo(function(t) {
-      e.setData({
-          userInfo: t
-      });
-  });
-},
-onload(){
-  this.getMyCount();
 }
 })
